a681519ccb607459f858066a627334aeb22b2a32
matiasmaldonado526 <matiasmaldonado526@gmail.com>
2026-03-11 16:08:54 +0000

    Se implementa la camara y se le actualiza para el buen funcionamiento xd--Gatomi

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+// src/app/(checkout)/scanner.tsx
+import { Camera, CameraView, useCameraPermissions } from 'expo-camera';
+import { Button, Text, View } from 'react-native';
 
-export default function Index() {
+export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Los permisos siguen cargando
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Los permisos no han sido otorgados
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ textAlign: 'center' }}>Necesitamos acceso a tu cámara</Text>
+        <Button onPress={requestPermission} title="Dar permiso" />
+      </View>
+    );
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }}>
+       <CameraView style={{ flex: 1 }} />
     </View>
   );
 }

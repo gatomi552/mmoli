574be86f1f490b3b86d3080ab090563891a4c3b5
matiasmaldonado526 <matiasmaldonado526@gmail.com>
2026-03-12 00:22:50 +0000

    Se Ajusta un tab para que se pueda mover entre -pantallas -y poder desactivar la camara de manera sencilla -ademas de colocar iconos en cada uno

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
-import { Stack } from "expo-router";
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <NativeTabs>
+
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon src={require('./assets/home.png')}  
+        />
+      </NativeTabs.Trigger>
+
+      <NativeTabs.Trigger name="Qr">
+        <Label>QR´S</Label>
+        <Icon src={require('./assets/Carpet.png')} 
+        />
+      </NativeTabs.Trigger>
+      {/*<NativeTabs.Trigger name="settings">
+      <Label hidden />
+        <Icon
+          sf="gearshape.fill"
+        />
+      </NativeTabs.Trigger>
+      */}
+    </NativeTabs>
+  );
 }
